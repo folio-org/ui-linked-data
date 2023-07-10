32a9f3d6c5d487b3ac3cea9e1ba1cd06cbf2b1d0
Siarhei Karol <skarol@ebsco.com>
2023-07-10 16:21:03 +0500

    Save and restore record

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -4,9 +4,10 @@ import './Input.scss';
 type InputProps = {
   placeholder?: string;
   value: string;
+  disabled?: boolean;
   onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
 };
 
-export const Input: FC<InputProps> = ({ placeholder, value, onChange }) => {
-  return <input className="input" value={value} placeholder={placeholder} onChange={onChange} />;
+export const Input: FC<InputProps> = ({ placeholder, value, disabled = false, onChange }) => {
+  return <input className="input" value={value} placeholder={placeholder} onChange={onChange} disabled={disabled} />;
 };
