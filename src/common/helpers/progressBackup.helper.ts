4a5b3cf1731adc40c4159cff4febdf569e0c35c4
Siarhei Karol <skarol@ebsco.com>
2023-07-11 18:31:59 +0500

    duplicate constant removed

--- src/common/helpers/progressBackup.helper.ts
+++ src/common/helpers/progressBackup.helper.ts
@@ -1,9 +1,8 @@
 import { PROFILE_IDS } from '../constants/bibframe.constants';
-
-export const NEW_RECORD_KEY = 'marva_new_record';
+import { DEFAULT_RECORD_ID } from '../constants/storage.constants';
 
 // TODO: define default profile that will be used for saving a new record
 export const generateRecordBackupKey = (
   profile: string = PROFILE_IDS.MONOGRAPH,
-  recordId: number | string = NEW_RECORD_KEY,
+  recordId: number | string = DEFAULT_RECORD_ID,
 ) => `${profile}:${recordId}`;
