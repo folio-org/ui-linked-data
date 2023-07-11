0e9672270b73a71d1d13cb611d93167324d673af
Siarhei Karol <skarol@ebsco.com>
2023-07-11 14:39:22 +0500

    build config updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
-import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
-import { env } from 'process'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
+import { env } from 'process';
 
-export default defineConfig(() => (
-  Number(env.EDITOR_BUILD_TYPE) ? {
-    plugins: [
-      react(),
-      cssInjectedByJsPlugin(),
-    ],
-    build: {
-      lib: {
-        entry: path.resolve(__dirname, 'src/embed.tsx'),
-        name: 'marva-next',
-        formats: ['es', 'umd'],
-        fileName: (format) => `marva-next.${format}.js`,
-      },
-    },
-    envPrefix: 'EDITOR_',
-  } : {
-    plugins: [react()],
-    envPrefix: 'EDITOR_',
-  }
-))
+export default defineConfig(() => {
+  return env.npm_config_type === 'library'
+    ? {
+        plugins: [react(), cssInjectedByJsPlugin()],
+
+        build: {
+          lib: {
+            entry: path.resolve(__dirname, 'src/embed.tsx'),
+            name: 'marva-next',
+            formats: ['es', 'umd'],
+            fileName: format => `marva-next.${format}.js`,
+          },
+        },
+
+        envPrefix: 'EDITOR_',
+      }
+    : {
+        plugins: [react()],
+        envPrefix: 'EDITOR_',
+      };
+});
