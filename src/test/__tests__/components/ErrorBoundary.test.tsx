be939d2c110d80370f0215f1a975a9af283046c3
Siarhei Karol <skarol@ebsco.com>
2023-07-19 15:59:33 +0500

    minor fixes after code review

--- src/test/__tests__/components/ErrorBoundary.test.tsx
+++ src/test/__tests__/components/ErrorBoundary.test.tsx
@@ -4,21 +4,18 @@ import { ErrorBoundary } from '../../../components/ErrorBoundary';
 
 describe('Error Boundary', () => {
   test('renders Error Boundary component', () => {
-    // Arrange
     // TODO: mock Error service
     console.error = jest.fn();
     const ThrowError = () => {
       throw new Error('Test error boundary');
     };
 
-    // Act
     render(
       <ErrorBoundary>
         <ThrowError />
       </ErrorBoundary>,
     );
 
-    // Assert
     expect(screen.getByTestId('errorBoundary')).toBeInTheDocument();
   });
 });
