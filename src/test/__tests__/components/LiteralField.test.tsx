70c9dc57846619835035a39b4d080f58666da4d0
nikolai <nikolai_litvinenko@epam.com>
2023-07-21 13:15:36 +0400

    feat: UILD-46: Jest/RTL Unit Tests for src/components/LiteralField

---
+++ src/test/__tests__/components/LiteralField.test.tsx
@@ -0,0 +1,27 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { v4 as uuidv4 } from 'uuid';
+import { LiteralField } from '@components/LiteralField';
+
+const displayName = 'displayName';
+const uuid = uuidv4();
+const onChangeFn = jest.fn();
+
+describe('Literal Field', () => {
+  beforeEach(() => render(<LiteralField displayName={displayName} uuid={uuid} onChange={onChangeFn} />));
+
+  test('renders Literal Field component', () => {
+    expect(screen.getByTestId('literal-field')).toBeInTheDocument();
+  });
+
+  test('triggers handleOnChange', () => {
+    const event = {
+      target: {
+        value: 'test',
+      },
+    };
+
+    fireEvent.change(screen.getByPlaceholderText(displayName), event);
+    expect(onChangeFn).toHaveBeenCalledWith(uuid, [{ label: event.target.value }]);
+  });
+});
