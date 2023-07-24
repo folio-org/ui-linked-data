b814caec982d2cb2f2e9dcbc883649ca20ea4e12
Siarhei Karol <skarol@ebsco.com>
2023-07-24 16:38:31 +0500

    add unit tests

---
+++ src/test/__tests__/common/helpers/progressBackup.helper.test.ts
@@ -0,0 +1,31 @@
+import * as ProgressBackupHelper from '@common/helpers/progressBackup.helper';
+
+describe('progressBackup.helper', () => {
+  describe('generateRecordBackupKey', () => {
+    function testGenerateRecordBackupKey(
+      profile: string | undefined,
+      recordId: number | string | undefined,
+      testResult: string,
+    ) {
+      const result = ProgressBackupHelper.generateRecordBackupKey(profile, recordId);
+
+      expect(result).toBe(testResult);
+    }
+
+    test('returns generated key with default params', () => {
+      testGenerateRecordBackupKey(undefined, undefined, 'lc:profile:bf2:Monograph:marva_new_record');
+    });
+
+    test('returns generated key with passed profile', () => {
+      testGenerateRecordBackupKey('testProfile', undefined, 'testProfile:marva_new_record');
+    });
+
+    test('returns generated key with passed recordID', () => {
+      testGenerateRecordBackupKey(undefined, 'testRecordId', 'lc:profile:bf2:Monograph:testRecordId');
+    });
+
+    test('returns generated key with all passed params', () => {
+      testGenerateRecordBackupKey('testProfile', 'testRecordId', 'testProfile:testRecordId');
+    });
+  });
+});
