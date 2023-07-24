3f02c80701b16b6944694043c082c60070ac0859
Siarhei Karol <skarol@ebsco.com>
2023-07-24 16:18:01 +0500

    disable Delete record button

--- src/components/RecordControls/RecordControls.tsx
+++ src/components/RecordControls/RecordControls.tsx
@@ -1,13 +1,18 @@
 import { memo, useState } from 'react';
+import { useRecoilValue } from 'recoil';
+import state from '@state';
 import { useRecordControls } from '@common/hooks/useRecordControls';
+import { DEFAULT_RECORD_ID } from '@common/constants/storage.constants';
 import { ModalCloseRecord } from '../ModalCloseRecord';
 import { ModalDeleteRecord } from '../ModalDeleteRecord';
 import './RecordControls.scss';
 
 export const RecordControls = memo(() => {
+  const record = useRecoilValue(state.inputs.record);
   const { saveRecord, deleteRecord, discardRecord } = useRecordControls();
   const [isCloseModalOpen, setIsCloseModalOpen] = useState(false);
   const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const isDisabled = !record || record?.id === DEFAULT_RECORD_ID;
 
   const onClickCloseButton = () => {
     setIsCloseModalOpen(true);
@@ -22,7 +27,9 @@ export const RecordControls = memo(() => {
       <div>
         <button onClick={saveRecord}>Save Record</button>
         <button onClick={onClickCloseButton}>Close Record</button>
-        <button onClick={onClickDeleteButton}>Delete Record</button>
+        <button onClick={onClickDeleteButton} disabled={isDisabled}>
+          Delete Record
+        </button>
       </div>
       <ModalCloseRecord
         isOpen={isCloseModalOpen}
