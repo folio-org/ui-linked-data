fca8c075c3678b5c432b4c93b2ec047ef3f229b6
Siarhei Karol <skarol@ebsco.com>
2023-07-27 09:54:13 +0500

    add tests for RecordControls

---
+++ src/test/__tests__/components/RecordControls.test.tsx
@@ -0,0 +1,25 @@
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { RecordControls } from '@components/RecordControls';
+
+jest.mock('@components/SaveRecord', () => ({
+  SaveRecord: () => <div data-testid="save-record-component" />,
+}));
+
+jest.mock('@components/CloseRecord', () => ({
+  CloseRecord: () => <div data-testid="close-record-component" />,
+}));
+
+jest.mock('@components/DeleteRecord', () => ({
+  DeleteRecord: () => <div data-testid="delete-record-component" />,
+}));
+
+describe('RecordControls', () => {
+  test('renders proper components', () => {
+    const { getByTestId } = render(<RecordControls />);
+
+    expect(getByTestId('save-record-component')).toBeInTheDocument();
+    expect(getByTestId('close-record-component')).toBeInTheDocument();
+    expect(getByTestId('delete-record-component')).toBeInTheDocument();
+  });
+});
