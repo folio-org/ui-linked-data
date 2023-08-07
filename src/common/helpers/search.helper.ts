9e272b46a0dfb6d312e6de75e6c8f35b18fe156a
nikolai <nlitvinenko@ebsco.com>
2023-08-07 16:40:44 +0400

    feat: UILD-68: Search results: response schema change

--- src/common/helpers/search.helper.ts
+++ src/common/helpers/search.helper.ts
@@ -2,31 +2,31 @@ import { ItemSearchResponse } from '@common/api/search.api';
 import { Row } from '@components/Table';
 import { alphabeticSortLabel } from './common.helper';
 
-const _TEMP_RESULT_MAX_AMOUNT = 10;
+const __TEMP_RESULT_MAX_AMOUNT = 10;
+const QUERY_DELIMITER = '=';
+const WILDCARD = '*';
 
-export const formatKnownItemSearchData = (result: ItemSearchResponse): Row[] | null =>
-  result?.content
-    ? result.content
-        .map(({ id, title, authors, dateOfPublication, editionStatement }) => ({
-          id: {
-            label: result?.search_query && result.search_query.split('=').at(-1),
-          },
-          title: {
-            label: title,
-          },
-          author: {
-            label: authors?.map(({ name }) => name).join('; '),
-          },
-          date: {
-            label: dateOfPublication,
-          },
-          edition: {
-            label: editionStatement,
-          },
-          __meta: {
-            id,
-          }
-        }))
-        .slice(0, _TEMP_RESULT_MAX_AMOUNT)
-        .sort((a, b) => alphabeticSortLabel(b?.date, a?.date))
-    : null;
+export const formatKnownItemSearchData = (result: ItemSearchResponse): Row[] =>
+  result.content
+    .map(({ id, title, contributors, publications, editionStatement }) => ({
+      id: {
+        label: result?.search_query && result.search_query.split(QUERY_DELIMITER).at(-1)?.replace(WILDCARD, ''),
+      },
+      title: {
+        label: title,
+      },
+      author: {
+        label: contributors?.map(({ name }) => name).join('; '),
+      },
+      date: {
+        label: publications?.map(({ dateOfPublication }) => dateOfPublication).join('; '),
+      },
+      edition: {
+        label: editionStatement,
+      },
+      __meta: {
+        id,
+      },
+    }))
+    .slice(0, __TEMP_RESULT_MAX_AMOUNT)
+    .sort((a, b) => alphabeticSortLabel(b?.date, a?.date));
