dc0b903926bf7942a85c284b1a3b89cd5a69389a
Siarhei Karol <135722306+SKarolFolio@users.noreply.github.com>
2023-08-23 11:14:35 +0300

    feat: UILD-77: render modal in portal (#63)

--- src/test/__mocks__/components/Modal.mock.tsx
+++ src/test/__mocks__/components/Modal.mock.tsx
@@ -1,3 +1,11 @@
+import { MODAL_CONTAINER_ID } from '@common/constants/uiElements.constants';
+
 jest.mock('@components/Modal', () => ({
   Modal: () => <div data-testid="modal-component" />,
 }));
+
+export const createModalContainer = () => {
+  const modalContainer = document.createElement('div');
+  modalContainer.setAttribute('id', MODAL_CONTAINER_ID);
+  document.body.appendChild(modalContainer);
+};
