6563fd05324682ebe856b21ace3ef920189ac0c0
Siarhei Karol <skarol@ebsco.com>
2023-09-19 17:03:37 +0500

    add Contributor support

--- src/common/constants/search.constants.ts
+++ src/common/constants/search.constants.ts
@@ -2,10 +2,12 @@ export enum Identifiers {
   ISBN = 'isbn',
   LCCN = 'lccn',
   TITLE = 'title',
+  CONTRIBUTOR = 'contributor',
 }
 
 export enum DisplayIdentifiers {
   isbn = 'marva.isbn',
   lccn = 'marva.lccn',
   title = 'marva.title',
+  contributor = 'marva.contributor',
 }
