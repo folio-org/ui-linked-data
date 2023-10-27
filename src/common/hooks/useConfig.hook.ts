0aada6f12af03e73e029ca2220b09cc07c1864f5
nikolai <nlitvinenko@ebsco.com>
2023-10-27 09:21:21 +0400

    feat: UILD-109: Story: Work Component - Edit mode| BF Thin Thread

--- src/common/hooks/useConfig.hook.ts
+++ src/common/hooks/useConfig.hook.ts
@@ -131,7 +131,7 @@ export const useConfig = () => {
       });
     } else {
       switch (type) {
-        // parent types
+        // parent types (i.e Monograph)
         case AdvancedFieldType.profile: {
           const { title, id, resourceTemplates } = (entry as ProfileEntry).json.Profile;
           const uuidArray = resourceTemplates.map(() => uuidv4());
@@ -162,6 +162,7 @@ export const useConfig = () => {
         }
         case AdvancedFieldType.hidden:
         case AdvancedFieldType.dropdownOption:
+        // i.e. Work, Instance, Item
         case AdvancedFieldType.block: {
           const { id, resourceURI, resourceLabel, propertyTemplates } = entry as ResourceTemplate;
           const { uriBFLite, uriWithSelector } = getUris(resourceURI, base, path);
@@ -292,7 +293,7 @@ export const useConfig = () => {
           return;
         }
         default: {
-          console.log('Not implemented.', entry);
+          console.error('Not implemented.', entry);
 
           return;
         }
