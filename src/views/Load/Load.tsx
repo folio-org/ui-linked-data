71ae28d39610e313977159e560ba2b052cc308a5
Siarhei Karol <skarol@ebsco.com>
2023-11-07 19:06:27 +0500

    code refactoring: add the custom hook

--- src/views/Load/Load.tsx
+++ src/views/Load/Load.tsx
@@ -6,22 +6,23 @@ import { generateEditResourceUrl } from '@common/helpers/navigation.helper';
 import { TYPE_URIS } from '@common/constants/bibframe.constants';
 import { Pagination } from '@components/Pagination';
 import './Load.scss';
+import { usePagination } from '@common/hooks/usePagination';
 
 type AvailableRecords = Record<string, any>[] | null | undefined;
-type PageMetadata = {
-  number: number;
-  totalElements: number;
-  totalPages: number;
-};
 
 export const Load = () => {
   const [availableRecords, setAvailableRecords] = useState<AvailableRecords>(null);
-  const [pageMetadata, setPageMetadata] = useState<PageMetadata>({ number: 0, totalElements: 0, totalPages: 0 });
-  const [currentPage, setCurrentPage] = useState(0);
+  const { getPageMetadata, setPageMetadata, getCurrentPageNumber, onPrevPageClick, onNextPageClick } = usePagination({
+    number: 0,
+    totalElements: 0,
+    totalPages: 0,
+  });
+  const currentPageNumber = getCurrentPageNumber();
+  const pageMetadata = getPageMetadata();
 
   useEffect(() => {
     getAllRecords({
-      pageNumber: currentPage,
+      pageNumber: currentPageNumber,
       type: TYPE_URIS.INSTANCE, // TODO: pass URI of the selected level of abstraction (Work, Instance, Item))
     })
       .then(res => {
@@ -29,7 +30,7 @@ export const Load = () => {
         setPageMetadata({ number: res.number, totalElements: res.total_elements, totalPages: res.total_pages });
       })
       .catch(err => console.error('Error fetching resource descriptions: ', err));
-  }, [currentPage]);
+  }, [currentPageNumber]);
 
   // TODO: Workaroud for demo; define type and format for data received from API
   const generateButtonLabel = ({ id, label }: RecordData) => {
@@ -46,22 +47,6 @@ export const Load = () => {
       </Link>
     ));
 
-  const onPrevPageClick = () => {
-    const prevPageNumber = currentPage - 1;
-
-    if (prevPageNumber < 0) return;
-
-    setCurrentPage(prevPageNumber);
-  };
-
-  const onNextPageClick = () => {
-    const nextPageNumber = currentPage + 1;
-
-    if (nextPageNumber > pageMetadata.totalPages - 1) return;
-
-    setCurrentPage(nextPageNumber);
-  };
-
   return (
     <div data-testid="load" className="load">
       <strong>
@@ -73,7 +58,7 @@ export const Load = () => {
             {renderRecords(availableRecords)}
             {pageMetadata.totalElements > 0 && (
               <Pagination
-                currentPage={currentPage}
+                currentPage={currentPageNumber}
                 totalPages={pageMetadata.totalPages}
                 onPrevPageClick={onPrevPageClick}
                 onNextPageClick={onNextPageClick}
