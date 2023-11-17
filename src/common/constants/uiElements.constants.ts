39ffec3b08bba6568b949ca51075b0c5055f4229
Siarhei Karol <skarol@ebsco.com>
2023-11-17 15:56:17 +0500

    add todo comment

--- src/common/constants/uiElements.constants.ts
+++ src/common/constants/uiElements.constants.ts
@@ -1,4 +1,4 @@
 export const MODAL_CONTAINER_ID = 'modal-container';
 
-// TODO: Could be passed as an environment variable
+// TODO: Can be passed as an environment variable or as the web component's attribute
 export const EMBEDDED_MODULE_CONTAINER_ID = 'ModuleContainer';
