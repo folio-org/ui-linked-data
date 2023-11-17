ec0ec75017a0881c09e2de2dce9a65202202a6bd
Siarhei Karol <skarol@ebsco.com>
2023-11-17 10:41:38 +0500

    update scrolling for embedded and standalone modes

--- src/common/constants/uiElements.constants.ts
+++ src/common/constants/uiElements.constants.ts
@@ -1,1 +1,4 @@
 export const MODAL_CONTAINER_ID = 'modal-container';
+
+// TODO: Could be passed as an environment variable
+export const EMBEDDED_MODULE_CONTAINER_ID = 'ModuleContainer';
--- src/common/helpers/pageScrolling.helper.ts
+++ src/common/helpers/pageScrolling.helper.ts
@@ -1,4 +1,8 @@
 import { SCROLL_BEHAVIOR, SCROLL_OFFSET } from '@common/constants/pageScrolling.constnts';
+import { EMBEDDED_MODULE_CONTAINER_ID } from '@common/constants/uiElements.constants';
+
+export const getScrollableEntity = () =>
+  __EMBEDDED_MODE__ ? document.getElementById(EMBEDDED_MODULE_CONTAINER_ID) : window;
 
 export const scrollElementIntoView = (elem?: Element | null, navElem?: Element | null) => {
   if (!elem) return;
@@ -6,13 +10,15 @@ export const scrollElementIntoView = (elem?: Element | null, navElem?: Element |
   const typedElem = elem as HTMLElement;
   const elementPosition = typedElem?.offsetTop || 0;
   const navHeight = navElem?.clientHeight || 0;
-
-  window.scrollTo({
-    top: elementPosition - navHeight - SCROLL_OFFSET,
+  const top = elementPosition - navHeight - SCROLL_OFFSET || 0;
+  const scrollOptions = {
+    top,
     behavior: SCROLL_BEHAVIOR,
-  });
+  } as ScrollToOptions;
+
+  getScrollableEntity()?.scrollTo(scrollOptions);
 };
 
 export const scrollToTop = () => {
-  window.scrollTo({ top: 0, behavior: SCROLL_BEHAVIOR });
+  getScrollableEntity()?.scrollTo({ top: 0, behavior: SCROLL_BEHAVIOR });
 };
