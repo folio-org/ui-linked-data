eafc123e1836f0c7f0d825e6a6ec0f306715b0de
Siarhei Karol <skarol@ebsco.com>
2024-03-07 17:49:17 +0500

    remove redundant inheritance

--- src/common/services/userValues/userValueTypes/complexLookup.ts
+++ src/common/services/userValues/userValueTypes/complexLookup.ts
@@ -1,16 +1,13 @@
-import { UserValueType } from './userValueType';
 import { IUserValueType } from './userValueType.interface';
 
-export class ComplexLookupUserValueService extends UserValueType implements IUserValueType {
+export class ComplexLookupUserValueService implements IUserValueType {
   generate(value: UserValueDTO) {
     const { data, uuid } = value;
     const typedData = data as string;
 
-    this.value = {
+    return {
       uuid: uuid || '',
       contents: [{ label: typedData }],
     };
-
-    return this.value;
   }
 }
--- src/common/services/userValues/userValueTypes/literal.ts
+++ src/common/services/userValues/userValueTypes/literal.ts
@@ -1,16 +1,13 @@
-import { UserValueType } from './userValueType';
 import { IUserValueType } from './userValueType.interface';
 
-export class LiteralUserValueService extends UserValueType implements IUserValueType {
+export class LiteralUserValueService implements IUserValueType {
   generate(value: UserValueDTO) {
     const { data, uuid } = value;
     const typedData = data as string | string[];
 
-    this.value = {
+    return {
       uuid: uuid || '',
       contents: [{ label: Array.isArray(typedData) ? typedData[0] : typedData }],
     };
-
-    return this.value;
   }
 }
