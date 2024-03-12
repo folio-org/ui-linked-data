64fdce7e8a31d0e8434cbb3a45be9b0c7109a024
Siarhei Karol <135722306+SKarolFolio@users.noreply.github.com>
2024-03-12 13:06:18 +0500

    UILD-214: Switch off the refactored code (#176)

--- src/common/constants/feature.constants.ts
+++ src/common/constants/feature.constants.ts
@@ -3,4 +3,4 @@ export const ADVANCED_SEARCH_ENABLED = true;
 export const SIMPLE_LOOKUPS_ENABLED = false;
 export const LOCALE_SELECT_ENABLED = false;
 
-export const IS_NEW_SCHEMA_BUILDING_ALGORITHM_ENABLED = true;
+export const IS_NEW_SCHEMA_BUILDING_ALGORITHM_ENABLED = false;
