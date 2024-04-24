efeafdbacf1ce74fe10227cf867be3d9a94acd35
Siarhei Karol <135722306+SKarolFolio@users.noreply.github.com>
2024-04-24 14:25:57 +0500

    UILD-259: search button unblocked (#222)

--- src/common/hooks/useLoadSearchResults.ts
+++ src/common/hooks/useLoadSearchResults.ts
@@ -1,6 +1,6 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { useSearchParams } from 'react-router-dom';
-import { useSetRecoilState } from 'recoil';
+import { useRecoilState, useSetRecoilState } from 'recoil';
 import { SearchQueryParams } from '@common/constants/routes.constants';
 import { SEARCH_RESULTS_LIMIT, SearchIdentifiers } from '@common/constants/search.constants';
 import state from '@state';
@@ -12,25 +12,46 @@ export const useLoadSearchResults = (
   const setData = useSetRecoilState(state.search.data);
   const setSearchBy = useSetRecoilState(state.search.index);
   const setQuery = useSetRecoilState(state.search.query);
+  const [forceRefresh, setForceRefresh] = useRecoilState(state.search.forceRefresh);
   const [searchParams] = useSearchParams();
-  const querySearchParam = searchParams.get(SearchQueryParams.Query);
-  const searchBySearchParam = searchParams.get(SearchQueryParams.SearchBy);
+  const queryParam = searchParams.get(SearchQueryParams.Query);
+  const searchByParam = searchParams.get(SearchQueryParams.SearchBy);
+  const prevSearchParams = useRef<{ query: string | null; searchBy: string | null }>({ query: null, searchBy: null });
+  const prevPageNumber = useRef(pageNumber);
 
   useEffect(() => {
-    if (searchBySearchParam) {
-      setSearchBy(searchBySearchParam as SearchIdentifiers);
+    async function makeSearch() {
+      const { query: prevQuery, searchBy: prevSearchBy } = prevSearchParams.current;
+
+      if (
+        prevQuery === queryParam &&
+        prevSearchBy === searchByParam &&
+        prevPageNumber.current === pageNumber &&
+        !forceRefresh
+      )
+        return;
+
+      if (searchByParam && prevSearchBy !== searchByParam) {
+        setSearchBy(searchByParam as SearchIdentifiers);
+      }
+
+      if (!queryParam) {
+        setData(null);
+        return;
+      }
+
+      // Sets query's value for Basic search
+      if (searchByParam && prevQuery !== queryParam) {
+        setQuery(queryParam);
+      }
+
+      await fetchData(queryParam, searchByParam as SearchIdentifiers, pageNumber * SEARCH_RESULTS_LIMIT);
+
+      setForceRefresh(false);
+      prevSearchParams.current = { query: queryParam, searchBy: searchByParam };
+      prevPageNumber.current = pageNumber;
     }
 
-    if (!querySearchParam) {
-      setData(null);
-      return;
-    }
-
-    // Sets query's value for Basic search
-    if (searchBySearchParam) {
-      setQuery(querySearchParam);
-    }
-
-    fetchData(querySearchParam, searchBySearchParam as SearchIdentifiers, pageNumber * SEARCH_RESULTS_LIMIT);
-  }, [querySearchParam, searchBySearchParam, pageNumber]);
+    makeSearch();
+  }, [queryParam, searchByParam, pageNumber, forceRefresh]);
 };
