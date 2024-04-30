4220b4d9fbb9adcb3bc0639de73c4fa199e7ffce
Siarhei Karol <135722306+SKarolFolio@users.noreply.github.com>
2024-04-30 10:15:07 +0500

    UILD-264: Disable trash icon (#234)

--- src/components/DuplicateGroup/DuplicateGroup.tsx
+++ src/components/DuplicateGroup/DuplicateGroup.tsx
@@ -1,5 +1,6 @@
 import { FC, memo } from 'react';
 import { Button, ButtonType } from '@components/Button';
+import { IS_DISABLED_FOR_ALPHA } from '@common/constants/feature.constants';
 import Plus16 from '@src/assets/plus-16.svg?react';
 import Trash16 from '@src/assets/trash-16.svg?react';
 import './DuplicateGroup.scss';
@@ -9,11 +10,11 @@ interface Props {
 }
 
 export const DuplicateGroup: FC<Props> = memo(({ onClick }) => (
-  <div className='duplicate-group'>
+  <div className="duplicate-group">
     <Button data-testid="id-duplicate-group" type={ButtonType.Icon} onClick={onClick}>
       <Plus16 />
     </Button>
-    <Button data-testid="id-delete-duplicate-group" type={ButtonType.Icon}>
+    <Button data-testid="id-delete-duplicate-group" type={ButtonType.Icon} disabled={IS_DISABLED_FOR_ALPHA}>
       <Trash16 />
     </Button>
   </div>
