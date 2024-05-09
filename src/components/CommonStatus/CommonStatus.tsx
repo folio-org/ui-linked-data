9414b65eda0c5f875577a3a6af63ffc2d5f84972
Siarhei Karol <skarol@ebsco.com>
2024-05-09 15:51:33 +0500

    update status messages

--- src/components/CommonStatus/CommonStatus.tsx
+++ src/components/CommonStatus/CommonStatus.tsx
@@ -3,10 +3,15 @@ import { useRecoilState } from 'recoil';
 import classNames from 'classnames';
 import { FormattedMessage } from 'react-intl';
 import state from '@state';
-import './CommonStatus.scss';
+import { StatusType } from '@common/constants/status.constants';
 import { Button, ButtonType } from '@components/Button';
+import CloseIcon from '@src/assets/times-16.svg?react';
+import CheckCircleIcon from '@src/assets/check-circle.svg?react';
+import WarningIcon from '@src/assets/exclamation-triangle.svg?react';
+import ErrorIcon from '@src/assets/exclamation-circle.svg?react';
+import './CommonStatus.scss';
 
-const DELETE_TIMEOUT = 5000;
+const DELETE_TIMEOUT = 10000;
 
 export const CommonStatus: FC = () => {
   const [statusMessages, setStatusMessages] = useRecoilState(state.status.commonMessages);
@@ -25,13 +30,29 @@ export const CommonStatus: FC = () => {
     setTimeout(deleteOldestMessage, DELETE_TIMEOUT);
   }, [statusMessages]);
 
+  const renderIcon = (type: StatusType) => {
+    switch (type) {
+      case StatusType.success:
+        return <CheckCircleIcon className="status-message-icon" />;
+      case StatusType.warning:
+        return <WarningIcon className="status-message-icon" />;
+      case StatusType.error:
+        return <ErrorIcon className="status-message-icon" />;
+      default:
+        return null;
+    }
+  };
+
   return statusMessages?.length ? (
     <div className="common-status" data-testid="common-status">
       {statusMessages.map(({ id, type, message }) => (
-        <div key={id} className={classNames(['status-message', type])}>
-          <FormattedMessage id={message as string} defaultMessage={message as string} />{' '}
-          <Button type={ButtonType.Text} onClick={() => deleteMessage(id)}>
-            x
+        <div key={id} className={classNames(['status-message', type])} role="status">
+          {renderIcon(type as StatusType)}
+          <span className="status-message-text">
+            <FormattedMessage id={message as string} defaultMessage={message as string} />
+          </span>
+          <Button className="status-message-close" type={ButtonType.Text} onClick={() => deleteMessage(id)}>
+            <CloseIcon />
           </Button>
         </div>
       ))}
