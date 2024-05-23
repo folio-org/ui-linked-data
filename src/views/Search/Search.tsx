6802e5a18ed6ab8671fe106054197eea5d1cae3a
nikolai <nlitvinenko@ebsco.com>
2024-05-23 18:19:29 +0400

    feat: UILD-266: Story: Linked data editor home page

--- src/views/Search/Search.tsx
+++ src/views/Search/Search.tsx
@@ -2,7 +2,7 @@ import { ItemSearch } from '@components/ItemSearch';
 import './Search.scss';
 
 export const Search = () => (
-  <div className="search" data-testid="search">
+  <div className="search" data-testid="search" id="ld-search-container">
     <ItemSearch /* fetchRecord={fetchRecord} */ />
   </div>
 );
