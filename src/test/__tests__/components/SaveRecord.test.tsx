8052ebff207e1d0672a2f8b459cd6afaad90a30a
Siarhei Karol <skarol@ebsco.com>
2024-06-21 16:19:43 +0500

    unit tests

--- src/test/__tests__/components/SaveRecord.test.tsx
+++ src/test/__tests__/components/SaveRecord.test.tsx
@@ -1,16 +1,40 @@
-import { saveRecord } from '@src/test/__mocks__/common/hooks/useRecordControls.mock';
+import { RecoilRoot } from 'recoil';
 import { render, screen, fireEvent } from '@testing-library/react';
+import { saveRecord } from '@src/test/__mocks__/common/hooks/useRecordControls.mock';
 import { SaveRecord } from '@components/SaveRecord';
+import state from '@state';
 
 describe('SaveRecord', () => {
-  beforeEach(() => render(<SaveRecord />));
+  function renderSaveRecordComponent(recordIsEdited = true, recordIsInititallyLoaded = true) {
+    render(
+      <RecoilRoot
+        initializeState={snapshot => {
+          snapshot.set(state.status.recordIsInititallyLoaded, recordIsInititallyLoaded);
+          snapshot.set(state.status.recordIsEdited, recordIsEdited);
+        }}
+      >
+        <SaveRecord primary />
+      </RecoilRoot>,
+    );
+  }
 
-  test('renders "Save record" button', () => {
-    expect(screen.getByTestId('save-record-button')).toBeInTheDocument();
+  test('renders enabled "Save and close" button', () => {
+    renderSaveRecordComponent();
+
+    expect(screen.getByTestId('save-record-and-close')).toBeInTheDocument();
+    expect(screen.getByTestId('save-record-and-close')).not.toBeDisabled();
+  });
+
+  test('renders disabled "Save and close" button', () => {
+    renderSaveRecordComponent(false);
+
+    expect(screen.getByTestId('save-record-and-close')).toBeDisabled();
   });
 
   test('triggers "saveRecord" function', () => {
-    fireEvent.click(screen.getByTestId('save-record-button'));
+    renderSaveRecordComponent();
+
+    fireEvent.click(screen.getByTestId('save-record-and-close'));
 
     expect(saveRecord).toHaveBeenCalledTimes(1);
   });
