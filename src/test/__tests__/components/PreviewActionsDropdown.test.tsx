541aacfcd5a579684a9ccf82fc8c18be58ad3e12
nikolai <nlitvinenko@ebsco.com>
2024-07-08 19:13:50 +0400

    feat: UILD-332: unit tests

---
+++ src/test/__tests__/components/PreviewActionsDropdown.test.tsx
@@ -0,0 +1,18 @@
+import '@src/test/__mocks__/common/hooks/useNavigateToEditPage.mock';
+import { navigateToEditPage } from '@src/test/__mocks__/common/hooks/useNavigateToEditPage.mock';
+import { fireEvent, render } from '@testing-library/react';
+import { PreviewActionsDropdown } from '@components/PreviewActionsDropdown';
+
+describe('PreviewActionsDropdown', () => {
+  const entityType = 'mockEntityType';
+  const referenceId = 'mockReferenceId';
+
+  it('navigates to edit page', () => {
+    const { getByRole, getByText } = render(<PreviewActionsDropdown entityType={entityType} referenceId={referenceId} />);
+
+    fireEvent.click(getByRole('button'));
+    fireEvent.click(getByText('marva.newInstance'))
+    
+    expect(navigateToEditPage).toHaveBeenCalled();
+  });
+});
