c34658f6e7682e49a14121190c8d111793118480
nikolai <nlitvinenko@ebsco.com>
2024-09-10 17:24:33 +0400

    enable save ctls for duplicate resource flow

--- src/components/SaveRecord/SaveRecord.tsx
+++ src/components/SaveRecord/SaveRecord.tsx
@@ -5,18 +5,21 @@ import { useRecordControls } from '@common/hooks/useRecordControls';
 import { Button, ButtonType } from '@components/Button';
 import state from '@state';
 import { useRecordStatus } from '@common/hooks/useRecordStatus';
+import { useSearchParams } from 'react-router-dom';
+import { QueryParams } from '@common/constants/routes.constants';
 
 const SaveRecord = ({ primary = false }) => {
-  const { saveRecord } = useRecordControls();
   const recordIsEdited = useRecoilValue(state.status.recordIsEdited);
+  const { saveRecord } = useRecordControls();
   const { hasBeenSaved } = useRecordStatus();
-  
+  const [searchParams] = useSearchParams();
+
   return (
     <Button
       data-testid={`save-record${primary ? '-and-close' : '-and-keep-editing'}`}
       type={primary ? ButtonType.Primary : ButtonType.Highlighted}
       onClick={() => saveRecord({ isNavigatingBack: primary })}
-      disabled={!hasBeenSaved && !recordIsEdited}
+      disabled={!searchParams.get(QueryParams.CloneOf) && !hasBeenSaved && !recordIsEdited}
     >
       <FormattedMessage id={!primary ? 'marva.saveAndKeepEditing' : 'marva.saveAndClose'} />
     </Button>
