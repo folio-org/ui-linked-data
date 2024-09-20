70477dba5a4839cfbde337c126e2eafbc83eb071
nikolai <nlitvinenko@ebsco.com>
2024-09-20 11:40:50 +0400

    unit: UILD-390: Implement Unique Selectors for E2E Testing

---
+++ src/test/__tests__/common/services/schema/entryPropertiesGenerator.service.test.ts
@@ -0,0 +1,17 @@
+import { EntryPropertiesGeneratorService } from '@common/services/schema/entryPropertiesGenerator.service';
+import * as schemaHelper from '@common/helpers/schema.helper';
+
+describe('EntryPropertiesGeneratorService', () => {
+  test('applies htmlId to entry', () => {
+    jest.spyOn(schemaHelper, 'getHtmlIdForEntry').mockImplementation(() => mockUuid);
+    
+    const mockUuid = 'mockUuid';
+    const mockSchema = new Map([[mockUuid, {} as SchemaEntry]]);
+    const service = new EntryPropertiesGeneratorService();
+
+    service.addEntryWithHtmlId(mockUuid);
+    service.applyHtmlIdToEntries(mockSchema);
+
+    expect(mockSchema.get(mockUuid)?.htmlId).toEqual(mockUuid);
+  });
+});
