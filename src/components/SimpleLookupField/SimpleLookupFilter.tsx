94ae8441cdf67d1be232e0d183b5c8311d2083fc
Siarhei Karol <skarol@ebsco.com>
2024-09-26 12:13:43 +0500

    simple lookup update

--- src/components/SimpleLookupField/SimpleLookupFilter.tsx
+++ src/components/SimpleLookupField/SimpleLookupFilter.tsx
@@ -8,55 +8,45 @@ import { SIMPLE_LOOKUPS_ENABLED } from '@common/constants/feature.constants';
 import { DropdownIndicator } from './DropdownIndicator';
 import { MultiValueRemove } from './MultiValueRemove';
 import { ClearIndicator } from './ClearIndicator';
+import { OptionMultiline } from './OptionMultiline';
 import { SimpleLookupFieldStyles } from './SimpleLookupField.styles';
 import './SimpleLookupField.scss';
 import state from '@state';
 
 interface Props {
   facet?: string;
-  value?: UserValueContents[];
   isDisabled?: boolean;
   id?: string;
   hasMappedSourceData?: boolean;
   onChange: (facet?: string, contents?: Array<UserValueContents>) => void;
 }
 
-export const SimpleLookupFilter: FC<Props> = ({
-  facet,
-  id,
-  value,
-  onChange,
-  hasMappedSourceData,
-  isDisabled = false,
-}) => {
+export const SimpleLookupFilter: FC<Props> = ({ facet, id, onChange, hasMappedSourceData, isDisabled = false }) => {
   const { simpleLookupRef, forceDisplayOptionsAtTheTop } = useSimpleLookupObserver();
   const sourceData = useRecoilValue(state.search.sourceData);
   const facetsData = useRecoilValue(state.search.facetsData);
   const { formatMessage } = useIntl();
 
-  let options = [] as SimpleLookupFilterOption[];
+  let options = [] as FilterLookupOption[];
 
   if (facet && facetsData && facetsData[facet]?.values) {
-    options = facetsData[facet]?.values?.map(option => {
-      const sourceElem = sourceData?.find(({ id }) => id === option.id);
-      const title = (hasMappedSourceData ? sourceElem?.name : option.id) || formatMessage({ id: 'marva.notSpecified' });
+    options = facetsData[facet]?.values?.map(({ id, totalRecords }) => {
+      const sourceElem = sourceData?.find(({ id: sourceDataId }) => sourceDataId === id);
+      const label = (hasMappedSourceData ? sourceElem?.name : id) || formatMessage({ id: 'marva.notSpecified' });
 
       return {
-        label: `${title} (${option.totalRecords})`,
-        value: id || '',
+        label,
+        subLabel: `(${totalRecords})`,
+        value: {
+          id: id || '',
+        },
       };
     });
   }
 
-  const [localValue, setLocalValue] = useState<any[]>(
-    // TODO: implement the options data structure
-    value?.map(({ label = '', meta: { uri, basicLabel } = {} }) => ({
-      value: { label: basicLabel || label, uri },
-      label,
-    })) || [],
-  );
+  const [localValue, setLocalValue] = useState<any[]>();
 
-  const handleOnChange = (options: MultiValue<MultiselectOption>) => {
+  const handleOnChange = (options: MultiValue<FilterLookupOption>) => {
     // TODO: implement the options selection
     const newValue = options.map<UserValueContents>(() => ({}));
 
@@ -77,13 +67,12 @@ export const SimpleLookupFilter: FC<Props> = ({
       openMenuOnFocus
       isMulti
       menuPlacement={forceDisplayOptionsAtTheTop ? 'top' : 'auto'}
-      components={{ DropdownIndicator, MultiValueRemove, ClearIndicator }}
+      components={{ DropdownIndicator, MultiValueRemove, ClearIndicator, Option: OptionMultiline }}
       isDisabled={isDisabled || !SIMPLE_LOOKUPS_ENABLED}
       options={options}
       onChange={handleOnChange as unknown as (newValue: unknown, actionMeta: ActionMeta<unknown>) => void}
       value={localValue}
       placeholder={<FormattedMessage id="marva.select" />}
-      loadingMessage={() => <FormattedMessage id="marva.loading" />}
       inputId="creatable-select-input"
       styles={SimpleLookupFieldStyles as unknown as StylesConfig<unknown, boolean, GroupBase<unknown>>}
       filterOption={createFilter({
