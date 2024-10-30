c9ea4b4b44af81aa57a4bcf70d557ef9533b9854
Siarhei Karol <skarol@ebsco.com>
2024-10-30 14:20:14 +0500

    remove unused const

--- src/common/i18n/locales.ts
+++ src/common/i18n/locales.ts
@@ -2,10 +2,3 @@ export const LOCALES = {
   BASE_ENGLISH: 'en',
   ENGLISH: 'en-US',
 };
-
-export const LOCALE_DISPLAY_NAMES: Record<string, string> = {
-  'en-US': 'English',
-  'ja-JA': '日本語 (Sample)',
-  'fr-FR': 'Français (Sample)',
-  'de-DE': 'Deutsch (Sample)',
-};
