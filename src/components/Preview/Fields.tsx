1cc6e76375958bd2c8a38a74f21c4fc3b9455fee
Siarhei Karol <skarol@ebsco.com>
2024-11-22 14:21:01 +0300

    fix Preview component

--- src/components/Preview/Fields.tsx
+++ src/components/Preview/Fields.tsx
@@ -1,4 +1,4 @@
-import { FC, memo, ReactNode } from 'react';
+import { ReactNode } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
 import classNames from 'classnames';
@@ -80,157 +80,165 @@ type FieldsProps = {
   forceRenderAllTopLevelEntities?: boolean;
 };
 
-export const Fields: FC<FieldsProps> = memo(
-  ({
-    base,
-    uuid,
-    paths,
-    level = 0,
-    altSchema,
-    altUserValues,
-    altDisplayNames,
-    hideActions,
-    forceRenderAllTopLevelEntities,
-  }) => {
-    const selectedEntries = useRecoilValue(state.config.selectedEntries);
-    const setRecordStatus = useSetRecoilState(state.status.recordStatus);
-    const record = useRecoilValue(state.inputs.record);
-    const currentlyPreviewedEntityBfid = useRecoilValue(state.ui.currentlyPreviewedEntityBfid);
-    const isEdited = useRecoilValue(state.status.recordIsEdited);
-    const userValuesFromState = useRecoilValue(state.inputs.userValues);
-    const schemaFromState = useRecoilValue(state.config.schema);
-    const userValues = altUserValues || userValuesFromState;
-    const schema = altSchema || schemaFromState;
+export const Fields = ({
+  base,
+  uuid,
+  paths,
+  level = 0,
+  altSchema,
+  altUserValues,
+  altDisplayNames,
+  hideActions,
+  forceRenderAllTopLevelEntities,
+}: FieldsProps) => {
+  const userValuesFromState = useRecoilValue(state.inputs.userValues);
+  const schemaFromState = useRecoilValue(state.config.schema);
+  const selectedEntries = useRecoilValue(state.config.selectedEntries);
+  const setRecordStatus = useSetRecoilState(state.status.recordStatus);
+  const record = useRecoilValue(state.inputs.record);
+  const currentlyPreviewedEntityBfid = useRecoilValue(state.ui.currentlyPreviewedEntityBfid);
+  const isEdited = useRecoilValue(state.status.recordIsEdited);
+  const { navigateToEditPage } = useNavigateToEditPage();
+  const userValues = altUserValues || userValuesFromState;
+  const schema = altSchema || schemaFromState;
 
-    const { navigateToEditPage } = useNavigateToEditPage();
+  const handleNavigateToEditPage = () => {
+    setRecordStatus({ type: isEdited ? RecordStatus.saveAndClose : RecordStatus.close });
 
-    const handleNavigateToEditPage = () => {
-      setRecordStatus({ type: isEdited ? RecordStatus.saveAndClose : RecordStatus.close });
+    const typedSelectedBlock = BFLITE_BFID_TO_BLOCK[bfid as keyof typeof BFLITE_BFID_TO_BLOCK];
+    const id = getRecordId(record, typedSelectedBlock.reference.uri, typedSelectedBlock.referenceKey);
 
-      const typedSelectedBlock = BFLITE_BFID_TO_BLOCK[bfid as keyof typeof BFLITE_BFID_TO_BLOCK];
-      const id = getRecordId(record, typedSelectedBlock.reference.uri, typedSelectedBlock.referenceKey);
+    navigateToEditPage(generateEditResourceUrl(id));
+  };
 
-      navigateToEditPage(generateEditResourceUrl(id));
-    };
+  const entry = base.get(uuid);
+  const isOnBranchWithUserValue = paths.includes(uuid);
+  const isEntity = level === ENTITY_LEVEL;
 
-    const entry = base.get(uuid);
-    const isOnBranchWithUserValue = paths.includes(uuid);
-    const isEntity = level === ENTITY_LEVEL;
+  if (!entry) return null;
 
-    if (!entry) return null;
+  const { displayName = '', children, type, bfid = '', path, linkedEntry, htmlId } = entry;
+  const isDependentDropdownOption =
+    type === AdvancedFieldType.dropdownOption && !!schema.get(getParentEntryUuid(path))?.linkedEntry?.controlledBy;
+  const visibleDropdownOption =
+    isDependentDropdownOption && selectedEntries.includes(uuid) ? <div>{displayName}</div> : null;
 
-    const { displayName = '', children, type, bfid = '', path, linkedEntry, htmlId } = entry;
-    const isDependentDropdownOption =
-      type === AdvancedFieldType.dropdownOption && !!schema.get(getParentEntryUuid(path))?.linkedEntry?.controlledBy;
-    const visibleDropdownOption =
-      isDependentDropdownOption && selectedEntries.includes(uuid) ? <div>{displayName}</div> : null;
+  if (visibleDropdownOption) return visibleDropdownOption;
 
-    if (visibleDropdownOption) return visibleDropdownOption;
+  // don't render empty dropdown options and their descendants
+  if (type === AdvancedFieldType.dropdownOption && !isOnBranchWithUserValue) return null;
 
-    // don't render empty dropdown options and their descendants
-    if (type === AdvancedFieldType.dropdownOption && !isOnBranchWithUserValue) return null;
+  // don't render top level entities not selected for preview
+  if (isEntity && !currentlyPreviewedEntityBfid.has(bfid) && !forceRenderAllTopLevelEntities) return null;
 
-    // don't render top level entities not selected for preview
-    if (isEntity && !currentlyPreviewedEntityBfid.has(bfid) && !forceRenderAllTopLevelEntities) return null;
+  const isPreviewable = !NOT_PREVIEWABLE_TYPES.includes(type as AdvancedFieldType);
+  const isGroupable = level <= GROUP_BY_LEVEL;
+  const hasChildren = children?.length;
+  const selectedUserValues = userValues[uuid];
+  const isBranchEnd = !hasChildren;
+  const isBranchEndWithoutValues = !selectedUserValues && isBranchEnd;
+  const isBranchEndWithValues = !!selectedUserValues;
+  const shouldRenderLabelOrPlaceholders =
+    (isPreviewable && isGroupable) ||
+    type === AdvancedFieldType.dropdown ||
+    (isBranchEndWithValues && type !== AdvancedFieldType.complex) ||
+    isBranchEndWithoutValues;
+  const hasOnlyDropdownChildren =
+    hasChildren &&
+    !children.filter(childUuid => schema.get(childUuid)?.type !== AdvancedFieldType.dropdownOption).length;
+  const shouldRenderValuesOrPlaceholders = !hasChildren || hasOnlyDropdownChildren;
+  const shouldRenderPlaceholders =
+    (isPreviewable && isGroupable && !isOnBranchWithUserValue) || !isOnBranchWithUserValue;
+  const isDependentDropdown = type === AdvancedFieldType.dropdown && !!linkedEntry?.controlledBy;
+  const displayNameWithAltValue =
+    altDisplayNames?.[displayName] ?? PREVIEW_ALT_DISPLAY_LABELS[displayName] ?? displayName;
+  const isBlock = level === GROUP_BY_LEVEL && shouldRenderLabelOrPlaceholders;
+  const isBlockContents = level === GROUP_CONTENTS_LEVEL;
+  const isInstance = bfid === PROFILE_BFIDS.INSTANCE;
+  const showEntityActions = !hideActions && isEntity;
+  const wrapEntities = forceRenderAllTopLevelEntities && isEntity;
 
-    const isPreviewable = !NOT_PREVIEWABLE_TYPES.includes(type as AdvancedFieldType);
-    const isGroupable = level <= GROUP_BY_LEVEL;
-    const hasChildren = children?.length;
-    const selectedUserValues = userValues[uuid];
-    const isBranchEnd = !hasChildren;
-    const isBranchEndWithoutValues = !selectedUserValues && isBranchEnd;
-    const isBranchEndWithValues = !!selectedUserValues;
-    const shouldRenderLabelOrPlaceholders =
-      (isPreviewable && isGroupable) ||
-      type === AdvancedFieldType.dropdown ||
-      (isBranchEndWithValues && type !== AdvancedFieldType.complex) ||
-      isBranchEndWithoutValues;
-    const hasOnlyDropdownChildren =
-      hasChildren &&
-      !children.filter(childUuid => schema.get(childUuid)?.type !== AdvancedFieldType.dropdownOption).length;
-    const shouldRenderValuesOrPlaceholders = !hasChildren || hasOnlyDropdownChildren;
-    const shouldRenderPlaceholders =
-      (isPreviewable && isGroupable && !isOnBranchWithUserValue) || !isOnBranchWithUserValue;
-    const isDependentDropdown = type === AdvancedFieldType.dropdown && !!linkedEntry?.controlledBy;
-    const displayNameWithAltValue =
-      altDisplayNames?.[displayName] ?? PREVIEW_ALT_DISPLAY_LABELS[displayName] ?? displayName;
-    const isBlock = level === GROUP_BY_LEVEL && shouldRenderLabelOrPlaceholders;
-    const isBlockContents = level === GROUP_CONTENTS_LEVEL;
-    const isInstance = bfid === PROFILE_BFIDS.INSTANCE;
-    const showEntityActions = !hideActions && isEntity;
-    const wrapEntities = forceRenderAllTopLevelEntities && isEntity;
+  return (
+    <ConditionalWrapper
+      condition={isBlock || isBlockContents || wrapEntities}
+      wrapper={getPreviewWrapper({ isBlock, isBlockContents, wrapEntities })}
+    >
+      {shouldRenderLabelOrPlaceholders && (
+        <strong
+          className={classNames({
+            'entity-heading': isEntity,
+            'sub-heading': isBlock,
+            'value-heading': !isGroupable,
+          })}
+        >
+          {isEntity && !isInstance && !altDisplayNames && <Lightbulb16 />}
+          {displayNameWithAltValue}
+          {showEntityActions && !isInstance && (
+            <Button type={ButtonType.Primary} className="toggle-entity-edit" onClick={handleNavigateToEditPage}>
+              <FormattedMessage id={`ld.edit${RESOURCE_TEMPLATE_IDS[bfid]}`} />
+            </Button>
+          )}
+          {showEntityActions && isInstance && (
+            <PreviewActionsDropdown
+              ownId={getRecordId(record, BLOCKS_BFLITE.WORK.uri, BLOCKS_BFLITE.INSTANCE.referenceKey)}
+              referenceId={getRecordId(record, BLOCKS_BFLITE.WORK.uri)}
+              entityType={BLOCKS_BFLITE.INSTANCE.resourceType}
+              handleNavigateToEditPage={handleNavigateToEditPage}
+            />
+          )}
+        </strong>
+      )}
+      {shouldRenderValuesOrPlaceholders &&
+        (userValues[uuid]
+          ? userValues[uuid]?.contents?.map(({ label, meta: { uri, parentUri, basicLabel } = {} } = {}) => {
+              if (!label && !basicLabel) return;
 
-    return (
-      <ConditionalWrapper
-        condition={isBlock || isBlockContents || wrapEntities}
-        wrapper={getPreviewWrapper({ isBlock, isBlockContents, wrapEntities })}
-      >
-        {shouldRenderLabelOrPlaceholders && (
-          <strong
-            className={classNames({
-              'entity-heading': isEntity,
-              'sub-heading': isBlock,
-              'value-heading': !isGroupable,
-            })}
+              const selectedLabel = basicLabel ?? label;
+
+              return (
+                selectedLabel && (
+                  <div key={`${selectedLabel}${uri}`}>
+                    {uri || parentUri ? (
+                      <a className="preview-value-link" target="blank" href={uri ?? parentUri}>
+                        {selectedLabel}
+                      </a>
+                    ) : (
+                      <>{selectedLabel}</>
+                    )}
+                  </div>
+                )
+              );
+            })
+          : shouldRenderPlaceholders &&
+            !isDependentDropdown && (
+              <div id={htmlId} className="value-group-wrapper">
+                -
+              </div>
+            ))}
+      {children?.map(uuid => {
+        const schemaEntry = schema.get(uuid);
+
+        return (
+          <ConditionalWrapper
+            key={uuid}
+            condition={!isGroupable && checkShouldGroupWrap(level, schemaEntry)}
+            wrapper={getValueGroupWrapper({ schemaEntry })}
           >
-            {isEntity && !isInstance && !altDisplayNames && <Lightbulb16 />}
-            {displayNameWithAltValue}
-            {showEntityActions && !isInstance && (
-              <Button type={ButtonType.Primary} className="toggle-entity-edit" onClick={handleNavigateToEditPage}>
-                <FormattedMessage id={`ld.edit${RESOURCE_TEMPLATE_IDS[bfid]}`} />
-              </Button>
-            )}
-            {showEntityActions && isInstance && (
-              <PreviewActionsDropdown
-                ownId={getRecordId(record, BLOCKS_BFLITE.WORK.uri, BLOCKS_BFLITE.INSTANCE.referenceKey)}
-                referenceId={getRecordId(record, BLOCKS_BFLITE.WORK.uri)}
-                entityType={BLOCKS_BFLITE.INSTANCE.resourceType}
-                handleNavigateToEditPage={handleNavigateToEditPage}
-              />
-            )}
-          </strong>
-        )}
-        {shouldRenderValuesOrPlaceholders &&
-          (userValues[uuid]
-            ? userValues[uuid]?.contents?.map(({ label, meta: { uri, parentUri, basicLabel } = {} } = {}) => {
-                if (!label && !basicLabel) return;
-
-                const selectedLabel = basicLabel ?? label;
-
-                return (
-                  selectedLabel && (
-                    <div key={`${selectedLabel}${uri}`}>
-                      {uri || parentUri ? (
-                        <a className="preview-value-link" target="blank" href={uri ?? parentUri}>
-                          {selectedLabel}
-                        </a>
-                      ) : (
-                        <>{selectedLabel}</>
-                      )}
-                    </div>
-                  )
-                );
-              })
-            : shouldRenderPlaceholders &&
-              !isDependentDropdown && (
-                <div id={htmlId} className="value-group-wrapper">
-                  -
-                </div>
-              ))}
-        {children?.map(uuid => {
-          const schemaEntry = schema.get(uuid);
-
-          return (
-            <ConditionalWrapper
+            <Fields
               key={uuid}
-              condition={!isGroupable && checkShouldGroupWrap(level, schemaEntry)}
-              wrapper={getValueGroupWrapper({ schemaEntry })}
-            >
-              <Fields key={uuid} uuid={uuid} base={base} paths={paths} level={level + 1} />
-            </ConditionalWrapper>
-          );
-        })}
-      </ConditionalWrapper>
-    );
-  },
-);
+              uuid={uuid}
+              base={base}
+              paths={paths}
+              level={level + 1}
+              altSchema={altSchema}
+              altUserValues={altUserValues}
+              altDisplayNames={altDisplayNames}
+              hideActions={hideActions}
+              forceRenderAllTopLevelEntities={forceRenderAllTopLevelEntities}
+            />
+          </ConditionalWrapper>
+        );
+      })}
+    </ConditionalWrapper>
+  );
+};
