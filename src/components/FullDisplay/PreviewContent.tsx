65d07312c7b1abdeea0fc22c3cb5091c7c1c48cf
Siarhei Karol <135722306+SKarolFolio@users.noreply.github.com>
2024-11-22 12:32:16 +0500

    UILD-410: Full Display refactoring to reduce nesting of functions (#40)

---
+++ src/components/FullDisplay/PreviewContent.tsx
@@ -0,0 +1,51 @@
+import { useRecoilState } from 'recoil';
+import { FormattedMessage } from 'react-intl';
+import { RESOURCE_TEMPLATE_IDS } from '@common/constants/bibframe.constants';
+import { generateEditResourceUrl } from '@common/helpers/navigation.helper';
+import { useNavigateToEditPage } from '@common/hooks/useNavigateToEditPage';
+import { Button, ButtonType } from '@components/Button';
+import { Preview } from '@components/Preview';
+import Times16 from '@src/assets/times-16.svg?react';
+import state from '@state';
+import './FullDisplay.scss';
+
+export const PreviewContent = () => {
+  const [previewContent, setPreviewContent] = useRecoilState(state.inputs.previewContent);
+  const { navigateToEditPage } = useNavigateToEditPage();
+
+  return previewContent.map(({ id, base, userValues, initKey, title, entities }) => {
+    const resourceType = entities?.map(e => RESOURCE_TEMPLATE_IDS[e])?.[0];
+    const resourceTypeWithFallback = resourceType ?? '-';
+    const handleButtonClick = () => setPreviewContent(previewContent.filter(entry => entry.id !== id));
+
+    return (
+      <div key={id}>
+        <div className="full-display-control-panel">
+          <Button className="close" data-testid="preview-remove" onClick={handleButtonClick}>
+            <Times16 />
+          </Button>
+          <div className="info">
+            <span className="title">{title}</span>
+            <span className="type">{resourceTypeWithFallback}</span>
+          </div>
+          <Button
+            data-testid="preview-fetch"
+            onClick={() => navigateToEditPage(generateEditResourceUrl(id))}
+            type={ButtonType.Highlighted}
+          >
+            <FormattedMessage id={`ld.edit${resourceType}`} defaultMessage="Edit" />
+          </Button>
+        </div>
+        {Object.keys(userValues).length ? (
+          <div data-testid="preview-contents-container" className="preview-contents-container">
+            <Preview altSchema={base} altUserValues={userValues} altInitKey={initKey} headless hideActions />
+          </div>
+        ) : (
+          <div>
+            <FormattedMessage id="ld.resourceWithIdIsEmpty" values={{ id }} />
+          </div>
+        )}
+      </div>
+    );
+  });
+};
