4ac14fb83770e571100ca41832b5a441d5579d2d
Siarhei Karol <skarol@ebsco.com>
2025-02-13 17:42:41 +0300

    add Modal component

--- src/components/SaveRecord/SaveRecord.tsx
+++ src/components/SaveRecord/SaveRecord.tsx
@@ -1,23 +1,30 @@
-import { memo } from 'react';
+import { FC, memo } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { useSearchParams } from 'react-router-dom';
-import { useRecordControls } from '@common/hooks/useRecordControls';
 import { Button, ButtonType } from '@components/Button';
 import { useRecordStatus } from '@common/hooks/useRecordStatus';
 import { QueryParams } from '@common/constants/routes.constants';
 import { useStatusState } from '@src/store';
 
-const SaveRecord = ({ primary = false }) => {
+type SaveRecordProps = {
+  primary?: boolean;
+  onSaveRecord: (isNavigatingBack: boolean) => void;
+};
+
+const SaveRecord: FC<SaveRecordProps> = ({ primary = false, onSaveRecord }) => {
   const { isRecordEdited } = useStatusState();
-  const { saveRecord } = useRecordControls();
   const { hasBeenSaved } = useRecordStatus();
   const [searchParams] = useSearchParams();
+
+  const handleButtonClick = () => {
+    onSaveRecord(primary);
+  };
 
   return (
     <Button
       data-testid={`save-record${primary ? '-and-close' : '-and-keep-editing'}`}
       type={primary ? ButtonType.Primary : ButtonType.Highlighted}
-      onClick={() => saveRecord({ isNavigatingBack: primary })}
+      onClick={handleButtonClick}
       disabled={!searchParams.get(QueryParams.CloneOf) && !hasBeenSaved && !isRecordEdited}
     >
       <FormattedMessage id={!primary ? 'ld.saveAndKeepEditing' : 'ld.saveAndClose'} />
