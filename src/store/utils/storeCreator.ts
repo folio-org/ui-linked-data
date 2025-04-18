8c11c22f0910c22a6335cbdf978519f9aa8307b7
Siarhei Karol <skarol@ebsco.com>
2025-04-18 12:25:01 +0500

    Add "resetAllStores" function to clear the global store on web component disconnect

--- src/store/utils/storeCreator.ts
+++ src/store/utils/storeCreator.ts
@@ -1,12 +1,32 @@
-import { create, type StateCreator } from 'zustand';
+import { create as actualCreate, type StateCreator } from 'zustand';
 import { devtools } from 'zustand/middleware';
 import { IS_PROD_MODE } from '@common/constants/bundle.constants';
 
 const STORE_NAME = 'Linked Data Editor';
 
+const storeResetFunctions = new Set<() => void>();
+
 export type WithDevtools<T> = ReturnType<typeof devtools<T>>;
 
 export type StateCreatorTyped<T> = StateCreator<T, [['zustand/devtools', never]], []>;
 
+export const create = (<T>() =>
+  (stateCreator: StateCreator<T>) => {
+    const store = actualCreate(stateCreator);
+    const initialState = store.getInitialState();
+
+    storeResetFunctions.add(() => {
+      store.setState(initialState, true);
+    });
+
+    return store;
+  }) as typeof actualCreate;
+
 export const generateStore = <T>(store: StateCreatorTyped<T>, name: string) =>
   create<T>()(devtools(store, { name: STORE_NAME, store: name, enabled: !IS_PROD_MODE }));
+
+export const resetAllStores = () => {
+  storeResetFunctions.forEach(reset => {
+    reset();
+  });
+};
