dba86c3df4548df73b52f91843e4b57b4b01ad01
Siarhei Karol <skarol@ebsco.com>
2025-05-01 18:14:05 +0500

    refactor: Enhance SchemaGeneratorService with improved node transformation and empty option handling

--- src/common/services/schema/schemaGenerator.service.ts
+++ src/common/services/schema/schemaGenerator.service.ts
@@ -5,6 +5,20 @@ import { ISelectedEntries } from '../selectedEntries/selectedEntries.interface';
 import { IEntryPropertiesGeneratorService } from './entryPropertiesGenerator.interface';
 import { ISchemaGenerator } from './schemaGenerator.interface';
 
+interface INodeTransformer {
+  transform(node: ProfileNode, uuid: string): TransformedNode;
+}
+
+interface TransformedNode extends SchemaEntry {
+  emptyOptionUuid?: string;
+}
+
+interface NodeOptions {
+  uuid: string;
+  path: string[];
+  children?: string[];
+  linkedEntry?: LinkedEntry;
+}
 export class SchemaGeneratorService implements ISchemaGenerator {
   private profile: Profile;
   private schema: Map<string, SchemaEntry>;
@@ -35,47 +49,68 @@ export class SchemaGeneratorService implements ISchemaGenerator {
     this.entryPropertiesGeneratorService?.applyHtmlIdToEntries(this.schema);
   }
 
-  private processNode(node: ProfileNode) {
+  private processNode(node: ProfileNode): void {
     const uuid = this.idToUuid.get(node.id) ?? '';
-    const { emptyOptionUuid, ...newNode } = this.createTransformedNode(node, uuid);
+    const transformedNode = this.createTransformedNode(node, uuid);
 
-    // Select first dropdown option if this is a dropdown node
-    if (newNode.type === AdvancedFieldType.dropdown && newNode.children?.length) {
-      const firstOptionId = newNode.children[0];
-
-      this.selectedEntriesService.addNew(undefined, firstOptionId);
-    }
-
-    // If this node has an empty option value, select it
-    if (emptyOptionUuid) {
-      this.selectedEntriesService.addNew(undefined, emptyOptionUuid);
-    }
-
-    this.entryPropertiesGeneratorService?.addEntryWithHtmlId(uuid);
-
-    this.schema.set(newNode.uuid, newNode);
+    this.handleNodeSelections(transformedNode);
+    this.addNodeToSchema(transformedNode);
   }
 
-  private createTransformedNode(node: ProfileNode, uuid: string) {
-    const transformedNode = {
-      ...node,
+  private handleNodeSelections(node: TransformedNode): void {
+    if (this.isDropdownWithChildren(node)) {
+      this.selectedEntriesService.addNew(undefined, node.children?.[0]);
+    }
+
+    if (node.emptyOptionUuid) {
+      this.selectedEntriesService.addNew(undefined, node.emptyOptionUuid);
+    }
+  }
+
+  private isDropdownWithChildren(node: TransformedNode): boolean {
+    return node.type === AdvancedFieldType.dropdown && Array.isArray(node.children) && node.children.length > 0;
+  }
+
+  private addNodeToSchema(node: TransformedNode): void {
+    const schemaNode = { ...node };
+    delete schemaNode.emptyOptionUuid;
+
+    this.entryPropertiesGeneratorService?.addEntryWithHtmlId(schemaNode.uuid);
+    this.schema.set(schemaNode.uuid, schemaNode);
+  }
+
+  private createTransformedNode(node: ProfileNode, uuid: string): TransformedNode {
+    const baseNode = this.createBaseNode(node, uuid);
+    return this.addEmptyOptionIfNeeded(baseNode);
+  }
+
+  private createBaseNode(node: ProfileNode, uuid: string): TransformedNode {
+    const nodeOptions: NodeOptions = {
       uuid,
       path: this.getPathForNode(node.id),
       children: this.transformChildren(node.children),
       linkedEntry: this.transformLinkedEntry(node.linkedEntry),
-    } as SchemaEntry & { emptyOptionUuid?: string };
+    };
 
-    // Add empty option for controlled fields
-    if (transformedNode.linkedEntry?.controlledBy) {
-      const emptyOptionUuid = generateEmptyValueUuid(uuid);
+    return {
+      ...node,
+      ...nodeOptions,
+    };
+  }
 
-      transformedNode.emptyOptionUuid = emptyOptionUuid;
-      transformedNode.children = transformedNode.children
-        ? [emptyOptionUuid, ...transformedNode.children]
-        : [emptyOptionUuid];
+  private addEmptyOptionIfNeeded(node: TransformedNode): TransformedNode {
+    if (!node.linkedEntry?.controlledBy) {
+      return node;
     }
 
-    return transformedNode;
+    const emptyOptionUuid = generateEmptyValueUuid(node.uuid);
+    const children = node.children ? [emptyOptionUuid, ...node.children] : [emptyOptionUuid];
+
+    return {
+      ...node,
+      emptyOptionUuid,
+      children,
+    };
   }
 
   private transformChildren(children?: string[]) {
