12d10a614ac55bcc5ff6c30ed3e47f58bba0e6ff
Ryan Lee <rlee1@ebsco.com>
2025-05-09 18:06:43 -0700

    Add dropzone file component to render display of an added file

---
+++ src/components/DropzoneFile/DropzoneFile.tsx
@@ -0,0 +1,38 @@
+import { FC } from 'react';
+import { Button } from '@components/Button';
+import Report16 from '@src/assets/report-16.svg?react';
+import Trash16 from '@src/assets/trash-16.svg?react';
+import './DropzoneFile.scss';
+
+interface Props {
+  file: File;
+  onRemoveFile: (file: File) => void;
+}
+
+export const DropzoneFile: FC<Props> = ({
+  file,
+  onRemoveFile,
+}) => {
+  const formatTimestamp = (timestamp: number) => {
+    return new Date(timestamp).toLocaleDateString();
+  };
+
+  return (
+    <div className='dropzone-file'>
+      <span className='file-info'>
+        <Report16 />
+        <span className='name'>
+          {file.name}
+        </span>
+        <span className='date'>
+          {formatTimestamp(file.lastModified)}
+        </span>
+      </span>
+      <Button
+        onClick={() => onRemoveFile(file)}
+      >
+        <Trash16 />
+      </Button>
+    </div>
+  );
+};
