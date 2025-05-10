0df9f3daf84a1123cdf28779ab07030c280c0509
Ryan Lee <rlee1@ebsco.com>
2025-05-09 18:06:03 -0700

    Add placeholder max file upload size

---
+++ src/common/api/import.api.ts
@@ -0,0 +1,23 @@
+import {
+  IMPORT_JSON_FILE_API_ENDPOINT,
+} from '@common/constants/api.constants';
+import baseApi from './base.api';
+
+export const importFile = async (files: File[]) => {
+  const formData = new FormData();
+
+  // Only uploading one file at the moment
+  if (files.length > 0) {
+    formData.append('fileName', files[0]);
+  }
+
+  const url = baseApi.generateUrl(IMPORT_JSON_FILE_API_ENDPOINT);
+
+  return baseApi.request({
+    url,
+    requestParams: {
+      method: 'POST',
+      body: formData
+    },
+  });
+};
--- src/common/constants/import.constants.ts
+++ src/common/constants/import.constants.ts
@@ -1,3 +1,6 @@
+// This will probably change in the future to something smaller than 50MiB
+export const MAX_FILE_SIZE_BYTES = 50 * 1024 * 1024 * 1024;
+
 export enum ImportModes {
   JsonFile = "jsonfile",
   JsonUrl = "jsonurl",
