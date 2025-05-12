6283056e9fee64f20972f36bef5c2e1cbef228ad
Ryan Lee <rlee1@ebsco.com>
2025-05-12 11:17:57 -0700

    Add basic spinner, from stripes with minor modifications from not having stripes common styles

---
+++ src/components/SpinnerEllipsis/SpinnerEllipsis.tsx
@@ -0,0 +1,9 @@
+import './SpinnerEllipsis.scss';
+
+export const SpinnerEllipsis = () => (
+  <div className='spinner'>
+    <div className='bounce1' />
+    <div className='bounce2' />
+    <div className='bounce3' />
+  </div>
+);
---
+++ src/components/SpinnerEllipsis/index.ts
@@ -0,0 +1,1 @@
+export { SpinnerEllipsis } from "./SpinnerEllipsis";
