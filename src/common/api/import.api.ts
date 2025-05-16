a0d2bc5156a4e8365952193853186072298fe731
Ryan Lee <rlee1@ebsco.com>
2025-05-16 13:20:43 -0700

    Clone the file and add a more rdf4j friendly type so it will be parsed properly

--- src/common/api/import.api.ts
+++ src/common/api/import.api.ts
@@ -1,4 +1,5 @@
 import { IMPORT_JSON_FILE_API_ENDPOINT } from '@common/constants/api.constants';
+import { LD_JSON_MIME_TYPE } from '@common/constants/import.constants';
 import baseApi from './base.api';
 
 export const importFile = async (files: File[]) => {
@@ -6,7 +7,9 @@ export const importFile = async (files: File[]) => {
 
   // Only uploading one file at the moment
   if (files.length > 0) {
-    formData.append('fileName', files[0]);
+    const buf = await files[0].arrayBuffer();
+    const blob = new Blob([new Uint8Array(buf)], { type: LD_JSON_MIME_TYPE });
+    formData.append('fileName', blob);
   }
 
   const url = baseApi.generateUrl(IMPORT_JSON_FILE_API_ENDPOINT);
