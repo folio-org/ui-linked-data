5aaeea32b786a3c8889903517f7bcaa2a5f4bcd3
Siarhei Karol <135722306+SKarolFolio@users.noreply.github.com>
2025-06-19 18:33:02 +0500

    UILD-555: Extended configs to add support for Subject of Work (#169)

--- src/configs/complexLookup/complexLookup.config.ts
+++ src/configs/complexLookup/complexLookup.config.ts
@@ -9,75 +9,97 @@ import { COMPLEX_LOOKUP_FILTERS_CONFIG } from './complexLookupFilters.config';
 import { COMPLEX_LOOKUP_SEARCH_BY_CONFIG } from './complexLookupSearchBy.config';
 import { COMPLEX_LOOKUP_SEARCHABLE_INDICES_MAP } from './complexLookupSeachableIndicesMap';
 
+const BASE_AUTHORITY_CONFIG = {
+  api: {
+    endpoints: {
+      base: '/search/authorities',
+      source: '/authority-source-files',
+      facets: '/search/authorities/facets',
+      bySearchSegment: {
+        [SearchSegment.Search]: '/search/authorities',
+        [SearchSegment.Browse]: '/browse/authorities',
+      },
+      marcPreview: '/source-storage/records/:recordId/formatted?idType=AUTHORITY',
+      validation: AUTHORITY_ASSIGNMENT_CHECK_API_ENDPOINT,
+    },
+    validationTarget: {
+      creator: AuthorityValidationTarget.CreatorOfWork,
+    },
+    sourceKey: 'authoritySourceFiles',
+    searchQuery: {
+      limit: 100,
+      precedingRecordsCount: 5,
+    },
+    results: {
+      containers: {
+        [SearchSegment.Search]: 'authorities',
+        [SearchSegment.Browse]: 'items',
+      },
+    },
+  },
+  labels: {
+    button: {
+      base: 'ld.assignAuthority',
+      change: 'ld.change',
+    },
+    modal: {
+      title: {
+        creator: 'ld.selectMarcAuthority',
+        contributor: 'ld.selectMarcAuthority',
+      },
+      searchResults: 'ld.marcAuthority',
+    },
+  },
+  // For displaying "Search" and "Browse" segments
+  segments: {
+    primary: {
+      [SearchSegment.Search]: {
+        type: SearchSegment.Search,
+        labelId: 'ld.search',
+        isVisiblePaginationCount: true,
+        isVisibleSubLabel: true,
+        isLoopedPagination: false,
+      },
+      [SearchSegment.Browse]: {
+        type: SearchSegment.Browse,
+        labelId: 'ld.browse',
+        isVisiblePaginationCount: false,
+        isVisibleSubLabel: false,
+        isLoopedPagination: true,
+      },
+    },
+    defaultValues: {
+      segment: SearchSegment.Browse,
+      searchBy: SearchableIndex.PersonalName,
+    },
+  },
+  // For displaying "Search by" control and "Filters"
+  searchBy: COMPLEX_LOOKUP_SEARCH_BY_CONFIG.authorities,
+  searchableIndicesMap: COMPLEX_LOOKUP_SEARCHABLE_INDICES_MAP,
+  filters: COMPLEX_LOOKUP_FILTERS_CONFIG.authorities,
+};
+
 export const COMPLEX_LOOKUPS_CONFIG: ComplexLookupsConfig = {
   [ComplexLookupType.Authorities]: {
+    ...BASE_AUTHORITY_CONFIG,
+    linkedField: 'subclass',
+  },
+  [ComplexLookupType.AuthoritiesSubject]: {
+    ...BASE_AUTHORITY_CONFIG,
     api: {
-      endpoints: {
-        base: '/search/authorities',
-        source: '/authority-source-files',
-        facets: '/search/authorities/facets',
-        bySearchSegment: {
-          [SearchSegment.Search]: '/search/authorities',
-          [SearchSegment.Browse]: '/browse/authorities',
-        },
-        marcPreview: '/source-storage/records/:recordId/formatted?idType=AUTHORITY',
-        validation: AUTHORITY_ASSIGNMENT_CHECK_API_ENDPOINT,
-      },
+      ...BASE_AUTHORITY_CONFIG.api,
       validationTarget: {
-        creator: AuthorityValidationTarget.CreatorOfWork,
-      },
-      sourceKey: 'authoritySourceFiles',
-      searchQuery: {
-        limit: 100,
-        precedingRecordsCount: 5,
-      },
-      results: {
-        containers: {
-          [SearchSegment.Search]: 'authorities',
-          [SearchSegment.Browse]: 'items',
-        },
+        subject: AuthorityValidationTarget.SubjectOfWork,
       },
     },
     labels: {
-      button: {
-        base: 'ld.assignAuthority',
-        change: 'ld.change',
-      },
+      ...BASE_AUTHORITY_CONFIG.labels,
       modal: {
+        ...BASE_AUTHORITY_CONFIG.labels.modal,
         title: {
-          creator: 'ld.selectMarcAuthority',
-          contributor: 'ld.selectMarcAuthority',
+          subject: 'ld.selectMarcAuthority',
         },
-        searchResults: 'ld.marcAuthority',
       },
     },
-    // For displaying "Search" and "Browse" segments
-    segments: {
-      primary: {
-        [SearchSegment.Search]: {
-          type: SearchSegment.Search,
-          labelId: 'ld.search',
-          isVisiblePaginationCount: true,
-          isVisibleSubLabel: true,
-          isLoopedPagination: false,
-        },
-        [SearchSegment.Browse]: {
-          type: SearchSegment.Browse,
-          labelId: 'ld.browse',
-          isVisiblePaginationCount: false,
-          isVisibleSubLabel: false,
-          isLoopedPagination: true,
-        },
-      },
-      defaultValues: {
-        segment: SearchSegment.Browse,
-        searchBy: SearchableIndex.PersonalName,
-      },
-    },
-    // For displaying "Search by" control and "Filters"
-    searchBy: COMPLEX_LOOKUP_SEARCH_BY_CONFIG.authorities,
-    searchableIndicesMap: COMPLEX_LOOKUP_SEARCHABLE_INDICES_MAP,
-    filters: COMPLEX_LOOKUP_FILTERS_CONFIG.authorities,
-    linkedField: 'subclass',
   },
 };
