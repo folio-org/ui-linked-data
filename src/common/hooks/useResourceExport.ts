adb09cb5a3fadad3eb37777167976821b095e1b7
Ryan Lee <rlee1@ebsco.com>
2025-06-27 15:40:05 -0700

    Handle errors for export, add tests for helper and new menu item

--- src/common/hooks/useResourceExport.ts
+++ src/common/hooks/useResourceExport.ts
@@ -1,12 +1,28 @@
 import { getRdfRecord } from '@common/api/records.api';
 import { initiateUserAgentDownload } from '@common/helpers/download.helper';
+import { StatusType } from '@common/constants/status.constants';
+import { UserNotificationFactory } from '@common/services/userNotification';
+import { useLoadingState, useStatusState } from '@src/store';
 
 export const useResourceExport = () => {
+  const { setIsLoading } = useLoadingState();
+  const { addStatusMessagesItem } = useStatusState();
+
   const exportInstanceRdf = async (resourceId: string) => {
-    const response = await getRdfRecord(resourceId);
-    if (response?.ok) {
-      const rdf = await response.blob();
-      initiateUserAgentDownload(rdf, `${resourceId}.json`);
+    try {
+      setIsLoading(true);
+
+      const response = await getRdfRecord(resourceId);
+      if (response?.ok) {
+        const rdf = await response.blob();
+        initiateUserAgentDownload(rdf, `${resourceId}.json`);
+      } else {
+        throw 'Invalid response';
+      }
+    } catch {
+      addStatusMessagesItem?.(UserNotificationFactory.createMessage(StatusType.error, 'ld.errorExportingRdf'));
+    } finally {
+      setIsLoading(false);
     }
   };
 
