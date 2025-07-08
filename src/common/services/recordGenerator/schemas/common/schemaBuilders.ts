fcabd4c7b70f977a8bbe4b0819788d27578215c8
Siarhei Karol <skarol@ebsco.com>
2025-07-08 16:48:13 +0500

    Fix formatting in createNotesProperty by adding a missing comma

--- src/common/services/recordGenerator/schemas/common/schemaBuilders.ts
+++ src/common/services/recordGenerator/schemas/common/schemaBuilders.ts
@@ -1,6 +1,5 @@
 import { BFLITE_URIS } from '@common/constants/bibframeMapping.constants';
 import { RecordSchemaEntryType } from '@common/constants/recordSchema.constants';
-import { link } from 'fs';
 
 export function createObjectProperty(properties: Record<string, RecordSchemaEntry>, options = {}) {
   return {
@@ -38,7 +37,7 @@ export function createNotesProperty(mappingReference: Record<string, { uri?: str
         options: {
           mappedValues: mappingReference,
           defaultValue: BFLITE_URIS.NOTE,
-          linkedProperty: 'value'
+          linkedProperty: 'value',
         },
       },
       value: {
