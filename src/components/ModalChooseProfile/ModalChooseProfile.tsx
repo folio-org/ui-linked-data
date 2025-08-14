b65af2e749b3a8768a8319242742816a9c5cd21a
Siarhei Karol <skarol@ebsco.com>
2025-08-14 16:50:14 +0500

    Add dynamic label generation for profile selection and update translations

--- src/components/ModalChooseProfile/ModalChooseProfile.tsx
+++ src/components/ModalChooseProfile/ModalChooseProfile.tsx
@@ -12,6 +12,28 @@ interface Props {
   profiles: ProfileDTO[];
 }
 
+const getLabelId = ({
+  labels: { workChange, instanceChange, defaultSet },
+  profileSelectionType: { action, resourceType },
+}: {
+  labels: {
+    workChange: string;
+    instanceChange: string;
+    defaultSet: string;
+  };
+  profileSelectionType: ProfileSelectionType;
+}) => {
+  let labelId;
+
+  if (action === 'change') {
+    labelId = resourceType === 'work' ? workChange : instanceChange;
+  } else {
+    labelId = defaultSet;
+  }
+
+  return labelId;
+};
+
 export const ModalChooseProfile: FC<Props> = memo(
   ({ isOpen, profileSelectionType, onCancel, onSubmit, onClose, profiles }) => {
     const { formatMessage } = useIntl();
@@ -32,14 +54,34 @@ export const ModalChooseProfile: FC<Props> = memo(
       onSubmit(selectedValue);
     };
 
-    const labelSetAsDefault = formatMessage({ id: 'ld.modal.chooseResourceProfile.setAsDefault' });
+    const labelSelect = formatMessage({
+      id: getLabelId({
+        labels: {
+          workChange: 'ld.modal.chooseResourceProfile.workProfile',
+          instanceChange: 'ld.modal.chooseResourceProfile.instanceProfile',
+          defaultSet: 'ld.resourceProfile',
+        },
+        profileSelectionType,
+      }),
+    });
+    const labelSetAsDefault = formatMessage({
+      id: getLabelId({
+        labels: {
+          workChange: 'ld.modal.chooseResourceProfile.setDefaultWorkProfile',
+          instanceChange: 'ld.modal.chooseResourceProfile.setDefaultInstanceProfile',
+          defaultSet: 'ld.modal.chooseResourceProfile.setAsDefault',
+        },
+        profileSelectionType,
+      }),
+    });
+    const labelSubmit = formatMessage({ id: profileSelectionType.action === 'set' ? 'ld.create.base' : 'ld.change' });
 
     return (
       <Modal
         className="modal-choose-profile"
         isOpen={isOpen}
         title={formatMessage({ id: 'ld.newInstance' })}
-        submitButtonLabel={formatMessage({ id: 'ld.create.base' })}
+        submitButtonLabel={labelSubmit}
         cancelButtonLabel={formatMessage({ id: 'ld.cancel' })}
         onClose={onClose}
         onSubmit={handleSubmit}
@@ -51,15 +93,8 @@ export const ModalChooseProfile: FC<Props> = memo(
           </p>
           <div className="modal-content-controls">
             <div className="modal-content-controls-block">
-              <h4 className="modal-content-subheader">
-                <FormattedMessage id="ld.resourceProfile" />
-              </h4>
-              <select
-                name={formatMessage({ id: 'ld.resourceProfile' })}
-                id="select-profile"
-                onChange={onChange}
-                value={selectedValue}
-              >
+              <h4 className="modal-content-subheader">{labelSelect}</h4>
+              <select name={labelSelect} id="select-profile" onChange={onChange} value={selectedValue}>
                 {profiles?.map(({ id, name }) => (
                   <option key={id} value={id}>
                     {name}
@@ -78,9 +113,7 @@ export const ModalChooseProfile: FC<Props> = memo(
                   name={labelSetAsDefault}
                   aria-label={labelSetAsDefault}
                 />
-                <span>
-                  <FormattedMessage id="ld.modal.chooseResourceProfile.setAsDefault" />
-                </span>
+                <span>{labelSetAsDefault}</span>
               </label>
             </div>
 
