638df31fe0f774ebb561c96cb654362aa7c7dce3
Siarhei Karol <135722306+SKarolFolio@users.noreply.github.com>
2025-09-12 20:03:58 +0500

    UILD-628: Add profile selection for Work (#228)

--- src/test/__tests__/views/Search.test.tsx
+++ src/test/__tests__/views/Search.test.tsx
@@ -1,11 +1,20 @@
 import '@src/test/__mocks__/common/helpers/pageScrolling.helper.mock';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import { setInitialGlobalState } from '@src/test/__mocks__/store';
 import { useSearchStore, useUIStore } from '@src/store';
 import { Search } from '@views';
+import { TYPE_URIS } from '@common/constants/bibframe.constants';
+import { ResourceType } from '@common/constants/record.constants';
 
 jest.mock('@common/constants/build.constants', () => ({ IS_EMBEDDED_MODE: false }));
+
+const onCreateNewResource = jest.fn();
+jest.mock('@common/hooks/useNavigateToCreatePage', () => ({
+  useNavigateToCreatePage: () => ({
+    onCreateNewResource,
+  }),
+}));
 
 describe('Search', () => {
   beforeEach(() =>
@@ -54,6 +63,23 @@ describe('Search', () => {
 
       expect(mockResetFullDisplayComponentType).toHaveBeenCalled();
       expect(mockResetSelectedInstances).toHaveBeenCalled();
+    });
+  });
+
+  describe('Actions dropdown', () => {
+    test('calls onCreateNewResource when "Create a new resource" option is clicked', () => {
+      // Click the Actions dropdown button
+      fireEvent.click(screen.getByTestId('search-view-actions-dropdown'));
+
+      // Click the "Create a new resource" option
+      fireEvent.click(screen.getByTestId('search-view-actions-dropdown__option-ld.newResource'));
+
+      expect(onCreateNewResource).toHaveBeenCalledWith({
+        resourceTypeURL: TYPE_URIS.WORK,
+        queryParams: {
+          type: ResourceType.work,
+        },
+      });
     });
   });
 });
