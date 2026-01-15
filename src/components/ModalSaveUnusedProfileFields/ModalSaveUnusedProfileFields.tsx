ecbb2895aff9b7d7aae4f90e55a9d1843b175849
Ryan Lee <rlee1@ebsco.com>
2026-01-14 18:13:13 -0800

    Not yet ready for use

--- src/components/ModalSaveUnusedProfileFields/ModalSaveUnusedProfileFields.tsx
+++
@@ -1,6 +0,0 @@
-import { Modal } from '@components/Modal';
-
-export const ModalSaveUnusedProfileFields = () => {
-  //isManageProfileSettingsUnusedFieldsModalOpen
-  return <Modal isOpen={false} title="close" onClose={() => {}}></Modal>;
-};
