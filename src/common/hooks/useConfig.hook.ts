2f64ea2b29158537d58cda4b8a2ab989e5a327e7
Siarhei Karol <skarol@ebsco.com>
2026-02-24 15:36:22 +0500

    feat: update profileId retrieval logic in useConfig hook to prioritize record data

--- src/common/hooks/useConfig.hook.ts
+++ src/common/hooks/useConfig.hook.ts
@@ -131,7 +131,11 @@ export const useConfig = () => {
     if (recordData && Object.keys(recordData).length) {
       const block = editingRecordBlocks?.block as keyof typeof BibframeEntitiesMap;
       const reference = editingRecordBlocks?.reference?.key;
-      const profileId = profileIdParam ?? (recordData[block]?.profileId as string | number | null | undefined);
+      const recordProfileId = recordData[block]?.profileId as string | number | null | undefined;
+
+      // Use record's profileId if present;
+      // only fall back to URL param if there is no profileId (valid for the Create resource page)
+      const profileId = recordProfileId ?? profileIdParam;
       const referenceProfileId = (recordData[block]?.[reference as string] as unknown as RecursiveRecordSchema[])?.[0]
         ?.profileId as string;
       const resourceTypeValue = BibframeEntitiesMap[block];
