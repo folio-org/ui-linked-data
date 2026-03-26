ae5f49f16fa674b7e8b67a005278b3e7788a6b07
Ryan Lee <rlee1@ebsco.com>
2026-03-26 00:14:56 +0000

    Update dictionary lookup to take same origin argument, most lookups will be done with a local vocabularies API now instead of directly from id.loc.gov

--- src/common/helpers/api.helper.ts
+++ src/common/helpers/api.helper.ts
@@ -9,7 +9,10 @@ export const loadSimpleLookup = async (
   }
 
   for await (const uri of uris) {
-    const formattedUri = !uri.includes('.json') ? `${uri}.json` : uri;
+    let formattedUri = uri;
+    if (uri.startsWith('http://') || uri.startsWith('https://')) {
+      formattedUri = !uri.includes('.json') ? `${uri}.json` : uri;
+    }
     const data = await fetchSimpleLookup(formattedUri);
 
     return data;
@@ -21,10 +24,15 @@ const fetchSimpleLookup = async (url: string): Promise<any> => {
     url = url.replace('http://', 'https://');
   }
 
+  let sameOrigin = true;
+  if (url.startsWith('http://') || url.startsWith('https://')) {
+    sameOrigin = false;
+  }
+
   // if we use the memberOf there might be a id URL in the params, make sure its not https
   url = url.replace('memberOf=https://id.loc.gov/', 'memberOf=http://id.loc.gov/');
 
-  const response = await getLookupDict(url, url.includes('.rdf') || url.includes('.xml'));
+  const response = await getLookupDict(url, url.includes('.rdf') || url.includes('.xml'), sameOrigin);
 
   return response;
 };
