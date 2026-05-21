3f37d7125d5fb475bcbde9e8a2e2017a449f1e76
Siarhei Karol <skarol@ebsco.com>
2026-05-21 12:44:33 +0500

    refactor: Implement buildProcessedResource function for enhanced resource processing

--- src/features/resources/hooks/useResourceProcessing.ts
+++ src/features/resources/hooks/useResourceProcessing.ts
@@ -2,24 +2,15 @@ import { useCallback } from 'react';
 import { useIntl } from 'react-intl';
 import { useSearchParams } from 'react-router-dom';
 
-import { v4 as uuidv4 } from 'uuid';
-
 import { DUPLICATE_RESOURCE_TEMPLATE } from '@/common/constants/resourceTemplates.constants';
 import { QueryParams } from '@/common/constants/routes.constants';
-import { getProfileConfig } from '@/common/helpers/profile.helper';
-import {
-  getAdjustedRecordContents,
-  getEditingRecordBlocks,
-  getPrimaryEntitiesFromRecord,
-  getRecordTitle,
-} from '@/common/helpers/record.helper';
-import { applyIntlToTemplates, getReferenceIdsRaw } from '@/common/helpers/recordFormatting.helper';
+import { getEditingRecordBlocks } from '@/common/helpers/record.helper';
+import { applyIntlToTemplates } from '@/common/helpers/recordFormatting.helper';
 import { useSchemaPipeline } from '@/common/hooks/useSchemaPipeline';
-import { getUri } from '@/configs/resourceTypes';
 
 import { useLoadProfile, useLoadProfileSettings } from '@/features/profiles';
 
-import { extractProfileParams } from '../helpers/resourceParams';
+import { buildProcessedResource } from '../helpers/buildProcessedResource';
 import type { ProcessedResource } from '../types';
 
 type ProcessResourceParams = {
@@ -32,14 +23,7 @@ export const useResourceProcessing = () => {
   const typeParam = searchParams.get(QueryParams.Type);
   const profileIdParam = searchParams.get(QueryParams.ProfileId);
 
-  const {
-    userValuesService,
-    selectedEntriesService,
-    schemaGeneratorService,
-    schemaWithDuplicatesService,
-    recordNormalizingService,
-    recordToSchemaMappingService,
-  } = useSchemaPipeline();
+  const pipeline = useSchemaPipeline();
 
   const { loadProfile } = useLoadProfile();
   const { loadProfileSettings } = useLoadProfileSettings();
@@ -48,102 +32,27 @@ export const useResourceProcessing = () => {
   const processResource = useCallback(
     async ({ record, asClone = false }: ProcessResourceParams = {}): Promise<ProcessedResource | null> => {
       const recordData = record?.resource ?? {};
-
       const editingRecordBlocks =
         recordData && Object.keys(recordData).length ? getEditingRecordBlocks(recordData as RecordEntry) : undefined;
+      const block = editingRecordBlocks?.block;
 
-      const { profileId, referenceProfileId, resourceType } = extractProfileParams({
-        recordData,
+      const templateMetadata =
+        asClone && block
+          ? applyIntlToTemplates({ templates: DUPLICATE_RESOURCE_TEMPLATE[block], format: formatMessage })
+          : undefined;
+
+      return buildProcessedResource({
+        pipeline,
+        record,
         profileIdParam,
         typeParam,
-        editingRecordBlocks,
+        asClone,
+        templateMetadata,
+        loadProfile,
+        loadProfileSettings,
       });
-
-      const profileConfig = getProfileConfig({ resourceType, profileId, referenceProfileId });
-
-      const loadedProfiles = await Promise.all(profileConfig.ids.map(id => loadProfile(id)));
-      const selectedProfile = profileConfig.rootEntry
-        ? ([profileConfig.rootEntry, ...loadedProfiles.flat()] as Profile)
-        : loadedProfiles[0];
-
-      if (!selectedProfile) return null;
-
-      const profileSettings = await loadProfileSettings(
-        String(profileConfig.ids?.[0]),
-        selectedProfile,
-        getUri(resourceType),
-      );
-
-      const initKey = uuidv4();
-      const userValues: UserValues = {};
-
-      userValuesService.set(userValues);
-      selectedEntriesService.set([]);
-      schemaGeneratorService.init(selectedProfile, profileSettings);
-      schemaGeneratorService.generate(initKey);
-
-      let updatedSchema = schemaGeneratorService.get();
-      let updatedUserValues = userValues;
-      let selectedRecordBlocks: SelectedRecordBlocks | undefined;
-
-      if (recordData && Object.keys(recordData).length) {
-        const { block, reference } = editingRecordBlocks ?? {};
-        const { record: adjustedRecord } = getAdjustedRecordContents({
-          record: recordData as RecordEntry,
-          block,
-          reference,
-          asClone,
-        });
-
-        const recordBlocks = [block, reference?.uri] as RecordBlocksList;
-
-        const template =
-          asClone && block
-            ? applyIntlToTemplates({ templates: DUPLICATE_RESOURCE_TEMPLATE[block], format: formatMessage })
-            : undefined;
-
-        selectedRecordBlocks = { block, reference };
-        schemaWithDuplicatesService.set(updatedSchema);
-        recordNormalizingService.init(adjustedRecord, block, reference);
-
-        const normalizedRecord = recordNormalizingService.get();
-
-        await recordToSchemaMappingService.init({
-          schema: updatedSchema,
-          record: normalizedRecord,
-          recordBlocks,
-          templateMetadata: template,
-        });
-
-        updatedSchema = recordToSchemaMappingService.get();
-        updatedUserValues = userValuesService.getAllValues();
-      }
-
-      return {
-        schema: updatedSchema,
-        userValues: updatedUserValues,
-        initKey,
-        selectedEntries: selectedEntriesService.get(),
-        selectedRecordBlocks,
-        selectedProfile,
-        title: getRecordTitle(recordData as RecordEntry),
-        entities: getPrimaryEntitiesFromRecord(record as RecordEntry),
-        referenceIds: getReferenceIdsRaw(record as RecordEntry),
-      };
     },
-    [
-      typeParam,
-      profileIdParam,
-      userValuesService,
-      selectedEntriesService,
-      schemaGeneratorService,
-      schemaWithDuplicatesService,
-      recordNormalizingService,
-      recordToSchemaMappingService,
-      loadProfile,
-      loadProfileSettings,
-      formatMessage,
-    ],
+    [typeParam, profileIdParam, pipeline, loadProfile, loadProfileSettings, formatMessage],
   );
 
   return { processResource };
