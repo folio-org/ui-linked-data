1222bbe1a339fe241f7df4e3d25e79a6b4cebc6d
Siarhei Karol <skarol@ebsco.com>
2026-06-29 13:11:44 +0500

    feat: Update AuthorityLabelFormatter to use Button component for title display

--- src/features/search/ui/formatters/authorities/AuthorityLabelFormatter.tsx
+++ src/features/search/ui/formatters/authorities/AuthorityLabelFormatter.tsx
@@ -1,5 +1,7 @@
 import { FC } from 'react';
 
+import { Button, ButtonType } from '@/components/Button';
+
 interface AuthorityLabelFormatterProps {
   row: SearchResultsTableRow;
   onTitleClick?: (id: string) => void;
@@ -8,9 +10,21 @@ interface AuthorityLabelFormatterProps {
 export const AuthorityLabelFormatter: FC<AuthorityLabelFormatterProps> = ({ row }) => {
   const title = row.label?.label as string;
 
+  // TODO: add event handler to display preview
+  const onTitleClick = () => {};
+
   if (!title) {
     return <span />;
   }
 
-  return <span>{title}</span>;
+  return (
+    <Button
+      type={ButtonType.Link}
+      className="authority-link"
+      onClick={onTitleClick}
+      data-testid={`authority-preview-link-${row.__meta.id}`}
+    >
+      {title}
+    </Button>
+  );
 };
